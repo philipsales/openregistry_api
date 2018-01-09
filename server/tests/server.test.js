71beb5314302f410f498086f2124def1e0d0aea0
Kristhian Tiu <b.kristhian.tiu@gmail.com>
2018-01-09 14:07:33 +0800

    Remove duplicate case tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -14,30 +14,3 @@ describe('/api-docs', () => {
         });
     });
 });
-
-describe('/cases', () => {
-    describe('#GET /cases', () => {
-        it('should get all cases');
-        it('should return 401 when user is not authenticated');
-    });
-    
-    describe('#POST /cases', () => {
-        it('should create and update cases');
-        it('should return 401 when user is not authenticated');
-        it('should return 400 validation error with the field details when fields are incorrect');
-    });
-
-    describe('#GET /cases/:case_id', () => {
-        it('should get a specific case');
-        it('should return 401 when user is not authenticated');
-        it('should return 404 when case can not be found');
-        it('should return 404 when case is soft deleted');
-    });
-
-    describe('#DELETE /cases/:case_id', () => {
-        it('should delete a specific case');
-        it('should return 401 when user is not authenticated');
-        it('should return 404 when case is not existing');
-        it('should return 404 when case is already soft deleted');
-    });
-});
