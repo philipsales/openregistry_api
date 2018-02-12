a99b44ab5b872b86fb02d6b35b03fef995f1dfa0
Kristhian Tiu <b.kristhian.tiu@gmail.com>
2018-02-12 09:21:02 +0800

    add organization on saving of cases

--- cases/cases.js
+++ cases/cases.js
@@ -12,7 +12,7 @@ var {Case, CaseError} = require('../server/models/case');
 router.use(bodyParser.json());
 
 router.post('/', authenticate, (req, res) => {
-    var seed = _.pick(req.body, ['case_number', 'diagnosis', 'forms']);
+    var seed = _.pick(req.body, ['case_number', 'organization', 'diagnosis', 'forms']);
     var instance = new Case(seed);
 
     Case.findOneAndRemove({case_number : seed.case_number}).then(() => {
