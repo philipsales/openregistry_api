2d6528c4a960d386692a993de5efc90129db7ebb
Miko Chu <angelomycar@gmail.com>
2018-12-20 20:06:31 +0800

    add department create

--- departments/departments.js
+++ departments/departments.js
@@ -7,15 +7,23 @@ const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {authenticate} = require('../server/middleware/authenticate');
+var {Department} = require('../server/models/department');
 
 router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
-    return res.status(200).send([
-        {id: 0, name: 'General Surgery Department'},
-        {id: 1, name: 'Obstetric Gynecology Department'},
-        {id: 2, name: 'Institute of Biology'}
-    ]);
+    Department.find().then(departments => 
+        res.status(200).send(departments), 
+        error => res.status(400).send(error));
+});
+
+router.post('/', authenticate, (req, res) => {
+    var body = _.pick(req.body, ['name', 'description']);
+    var department = new Department(body);
+    department.save().then(new_department => 
+        res.status(200).send(new_department),
+        error => res.status(400).send(error)
+    );
 });
 
 module.exports = router
---
+++ server/models/department.js
@@ -0,0 +1,45 @@
+const _ = require('lodash');
+var mongoose = require('mongoose');
+
+var DepartmentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        trim: true
+    }
+});
+
+DepartmentSchema.methods.toJSON = function () {
+    var caseObject = this.toObject();
+    return _.pick(caseObject, ['_id', 'name', 'description']);
+}
+
+var handleDuplicateDepartment = function(error, res, next) {
+    if (error.name === 'MongoError' && error.code === 11000) {
+        next({
+            code: 400,
+            errors: [{
+                field: 'name',
+                error: 'duplicate'
+            }],
+            userMessage: 'Department already taken. Please choose another.',
+            internalMessage: 'duplicate department name on departments table'
+        });
+    } else {
+      next();
+    }
+};
+
+DepartmentSchema.post('save', handleDuplicateDepartment);
+DepartmentSchema.post('findOneAndUpdate', handleDuplicateDepartment);
+
+var Department = mongoose.model('Department', DepartmentSchema);
+
+module.exports = {
+    Department
+}
